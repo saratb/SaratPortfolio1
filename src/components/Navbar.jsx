e1a63ee413a4d27e6cf710259fbbca352fbc8f43
saratb <saratbondada2014@gmail.com>
2023-09-14 20:34:12 -0400

    Logo Main and Favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React , { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav 
+      className={`${styles.paddingX} w-full flx items-center py-5 foxed top-0 z-20 bg-primary`}>
+        <div className='w-full flex justify-between items-center max-w-7xl mx-auto'></div> 
+
+          <Link 
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}>
+
+            <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+
+          </Link>
+    </nav>
   )
 }
 
