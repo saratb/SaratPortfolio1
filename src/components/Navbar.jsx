b9f9f535f73c134a656c8d791c385dfd9bead045
saratb <saratbondada2014@gmail.com>
2023-09-14 20:43:08 -0400

    tailwind css NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
           }}>
 
             <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+            <p className="text-white text-[18px] font-bold cursor-pointer">Sarat <span className="sm:block hidden"> | Bondada </span></p>
 
           </Link>
     </nav>
@@ -27,3 +28,6 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+// npm install --legacy-peer-deps -D  postcss autoprefixer  
+// npx tailwindcss init -p
