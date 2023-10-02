f3ae324d4b858f08d3dd3d057519b8f9455e584a
saratb <saratbondada2014@gmail.com>
2023-10-01 21:26:45 -0400

    Hero Section Edit Begining

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ const App = () => { // Modern Function
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar /> 
+          <Navbar />
+          <Hero /> 
         </div>
           <Works />
           <Feedbacks />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import { motion } from 'framer-motion'
 
+import { styles } from '../styles'
+import { ComputerCanvas } from './canvas'
 const Hero = () => {
   return (
     <div>Hero</div>
